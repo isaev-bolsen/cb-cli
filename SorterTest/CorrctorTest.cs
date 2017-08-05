d95089e099ea18edbc7cebe42bc1b97cb194cd6a
isaev-bolsen <al_mamistov@mail.ru>
2017-08-05 10:35:48 +0300

    Test CreatedDateTime adding

---
+++ SorterTest/CorrctorTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PX.Api.ContractBased.Maintenance.Cli.Utils;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SorterTest
+{
+    [TestClass]
+    public class CorrctorTest
+    {
+        [TestMethod]
+        public void TestAddingTheCreatedDateTime()
+        {
+            XDocument Doc = XDocument.Load("Sample1.xml");
+            Doc.AddCreatedAndModifiedDateTime();
+
+            foreach (XElement Entity in Doc.Elements().Single().Elements("TopLevelEntity"))
+            {
+                XElement[] dateTimeFields = Entity.Elements("Field").Where(e => e.Attribute("type").Value == "DateTimeValue").ToArray();
+                Assert.IsTrue(dateTimeFields.Length >= 2);
+                dateTimeFields.Single(e => e.Attribute("name").Value == "CreatedDateTime");
+                dateTimeFields.Single(e => e.Attribute("name").Value == "LastModifiedDateTime");
+            }
+        }
+    }
+}
