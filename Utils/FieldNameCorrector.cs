110ab5cdcecf645045968f154f7e3efa7ba70466
Alexandr Mamistov <amamistov@acumatica.com>
2017-09-20 11:41:12 +0300

    Stub

---
+++ Utils/FieldNameCorrector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System;
+
+namespace PX.Api.ContractBased.Maintenance.Cli.Utils
+{
+    internal static class FieldNameCorrector
+    {
+        internal static void CorrectAttributeValue(XAttribute a)
+        {
+
+        }
+    }
+}
