e16556429d657107a0054d0c552e6d65d54e7557
Alexandr Mamistov <amamistov@acumatica.com>
2017-09-20 12:26:18 +0300

    First rule

---
+++ SorterTest/FieldNameCorrectorTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PX.Api.ContractBased.Maintenance.Cli.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SorterTest
+{
+    [TestClass]
+    public class FieldNameCorrectorTest
+    {
+        private static XAttribute GetAttribute(string value) => new XAttribute("field", value);
+
+        [TestMethod]
+        public void TestOriginal()
+        {
+            XAttribute attr = GetAttribute("OrigOrderNbr");
+            FieldNameCorrector.CorrectAttributeValue(attr);
+            Assert.AreEqual("OriginalOrderNbr", attr.Value);
+        }
+    }
+}
--- Utils/FieldNameCorrector.cs
+++ Utils/FieldNameCorrector.cs
@@ -7,14 +7,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using System;
+using System.Text.RegularExpressions;
 
 namespace PX.Api.ContractBased.Maintenance.Cli.Utils
 {
-    internal static class FieldNameCorrector
+    public static class FieldNameCorrector
     {
-        internal static void CorrectAttributeValue(XAttribute a)
+        private static Regex SplitByCamel = new Regex(@"(?<=[A-Z])(?=[A-Z][a-z]) | (?<=[^A-Z])(?=[A-Z]) | (?<=[A-Za-z])(?=[^A-Za-z])",
+            RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+        
+        public static void CorrectAttributeValue(XAttribute a)
         {
+            string[] segments = SplitByCamel.Split(a.Value);
 
+            if (segments[0] == "Orig") segments[0] = "Original";
+
+            a.Value = string.Concat(segments);
         }
     }
 }
